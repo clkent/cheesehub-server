65ef820876dacd0a9ae8fcd57969fd98e76bdc9e
clkent <chelsea.kent@gmail.com>
2018-10-13 23:11:24 -0700

    up

--- index.js
+++ index.js
@@ -3,18 +3,41 @@
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
 
 const { PORT, CLIENT_ORIGIN } = require('./config');
 const { dbConnect } = require('./db-mongoose');
+const Cheese = require('./models/cheese');
 // const {dbConnect} = require('./db-knex');
 
 const app = express();
+
+const cheeseList = [
+  'Bath Blue',
+  'Barkham Blue',
+  'Buxton Blue',
+  'Cheshire Blue',
+  'Devon Blue',
+  'Dorset Blue Vinney',
+  'Dovedale',
+  'Exmoor Blue',
+  'Harbourne Blue',
+  'Lanark Blue',
+  'Lymeswold',
+  'Oxford Blue',
+  'Shropshire Blue',
+  'Stichelton',
+  'Stilton',
+  'Blue Wensleydale',
+  'Yorkshire Blue'
+];
 
 app.use(
   morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev', {
     skip: (req, res) => process.env.NODE_ENV === 'test'
   })
 );
+app.use(express.json());
 
 app.use(
   cors({
@@ -23,25 +46,38 @@ app.use(
 );
 
 app.get('/api/cheeses', (req, res, next) => {
-  return res.json([
-    'Bath Blue',
-    'Barkham Blue',
-    'Buxton Blue',
-    'Cheshire Blue',
-    'Devon Blue',
-    'Dorset Blue Vinney',
-    'Dovedale',
-    'Exmoor Blue',
-    'Harbourne Blue',
-    'Lanark Blue',
-    'Lymeswold',
-    'Oxford Blue',
-    'Shropshire Blue',
-    'Stichelton',
-    'Stilton',
-    'Blue Wensleydale',
-    'Yorkshire Blue'
-  ]);
+  Cheese.find()
+    .then(results => {
+      res.json(results);
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
+// app.post('/api/cheeses', (req, res, next) => {
+//   const {cheese} = req.body;
+//   Cheese.create({cheese})
+//     .then(res.sendStatus(201))
+//     .catch(err => next(err));
+// });
+
+// Custom 404 Not Found route handler
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// Custom Error Handler
+app.use((err, req, res, next) => {
+  if (err.status) {
+    const errBody = Object.assign({}, err, { message: err.message });
+    res.status(err.status).json(errBody);
+  } else {
+    console.error(err);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
 });
 
 function runServer(port = PORT) {
---
+++ models/cheese.js
@@ -0,0 +1,16 @@
+'use strict';
+const mongoose = require('mongoose');
+
+const cheeseSchema = new mongoose.Schema({
+  cheese: { type: String, required: true }
+});
+
+cheeseSchema.set('toObject', {
+  virtuals: true,
+  transform: (doc, result) => {
+    delete result._id;
+    delete result.__v;
+  }
+});
+
+module.exports = mongoose.model('Cheese', cheeseSchema);
